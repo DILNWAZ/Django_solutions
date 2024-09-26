356e6cc6ece642aea3bd5e47d54ef35951c06b6d
DILNWAZ <dilnawazansari831@gmail.com>
2024-09-26 08:37:18 -0700

    WOW

--- pstask8 meta/meta/meta/urls.py
+++ pstask8 meta/meta/meta/urls.py
@@ -18,12 +18,17 @@ from django.contrib import admin
 from django.urls import path,include
 from myapp import views
 from rest_framework.routers import DefaultRouter
+# from rest_framework.authtoken.views import obtain_auth_token
+from myapp.auth import CustomAuthToken
 
 router = DefaultRouter()
 router.register('StudentApi',views.studentModelViewSet,basename = 'Student')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include(router.urls)),
-    path("auth/", include('rest_framework.urls'))
+    path("", include(router.urls)),
+    path("auth", include('rest_framework.urls')),
+    # path("gettoken/", obtain_auth_token),
+    path("gettoken/", CustomAuthToken.as_view()),
+
 ]
--- pstask8 meta/meta/myapp/views.py
+++ pstask8 meta/meta/myapp/views.py
@@ -2,19 +2,19 @@ from django.shortcuts import render
 from .models import Student
 from .serializer import Student_serializer
 from rest_framework import viewsets
-from rest_framework.authentication import SessionAuthentication
+from rest_framework.authentication import SessionAuthentication,TokenAuthentication
 from rest_framework.permissions import IsAuthenticated,IsAdminUser,AllowAny,IsAuthenticatedOrReadOnly,DjangoModelPermissions
 
 class studentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = Student_serializer
-    authentication_classes = [SessionAuthentication]
+    authentication_classes = [TokenAuthentication]
     # authentication_classes = [BasicAuthentication]
     # permission_classes = [IsAuthenticated]
     # permission_classes = [IsAuthenticatedOrReadOnly]
     # permission_classes = [AllowAny]
     # permission_classes = [IsAdminUser]
-    permission_classes = [DjangoModelPermissions]
+    permission_classes = [IsAuthenticated]
     
 
 
